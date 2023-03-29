51d3e89561fc4f2a721a51d563ee127a3ec6a08c
meethigher <meethigher@qq.com>
2023-03-29 18:15:08 +0800

    feat: 新增oracle、mysql、sqlserver、db2的client

--- src/main/java/top/meethigher/export/Args.java
+++ src/main/java/top/meethigher/export/Args.java
@@ -10,10 +10,8 @@ import top.meethigher.dbmonitor.model.sizemonitor.DBSize;
 import top.meethigher.dbmonitor.model.sizemonitor.TableSize;
 import top.meethigher.dbmonitor.model.structuremonitor.ColumnInfo;
 import top.meethigher.dbmonitor.model.structuremonitor.TableStructure;
-import top.meethigher.dbmonitor.service.DBSizeMonitor;
-import top.meethigher.dbmonitor.service.TableStructureMonitor;
-import top.meethigher.dbmonitor.service.impl.DefaultDBSizeMonitor;
-import top.meethigher.dbmonitor.service.impl.DefaultTableStructureMonitor;
+import top.meethigher.dbmonitor.service.DBMonitor;
+import top.meethigher.dbmonitor.service.DefaultDBMonitor;
 import top.meethigher.export.model.Database;
 
 import java.util.List;
@@ -29,9 +27,7 @@ public class Args {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(Args.class);
 
 
-    private static final DBSizeMonitor<DBMonitorProperty, DBSize> dbMonitor = new DefaultDBSizeMonitor();
-
-    private static final TableStructureMonitor<DBMonitorProperty, TableStructure> tbMonitor = new DefaultTableStructureMonitor();
+    private static final DBMonitor<DBSize, TableStructure> monitor = new DefaultDBMonitor();
 
 
     @Parameter(names = {"--host", "-h"}, description = "目标主机host")
@@ -55,7 +51,7 @@ public class Args {
     @Parameter(names = "--title", description = "文件标题")
     public String exportTitle = "数据库结构说明";
 
-    @Parameter(names = "-t", description = "数据库类型 0-postgresql 1-mysql 2-oracle 3-sqlserver")
+    @Parameter(names = "-t", description = "数据库类型 0-postgresql 1-mysql 2-oracle 3-sqlserver 4-db2")
     public int type = 0;
 
     @Parameter(names = "--help", help = true)
@@ -76,33 +72,33 @@ public class Args {
         db.setExportFileName(argsObj.exportFileName);
         db.setExportTitle(argsObj.exportTitle);
         DBMonitorProperty property = new DBMonitorProperty();
-        property.setDatabase(argsObj.db);
-        property.setHost(argsObj.host);
-        property.setPort(argsObj.port);
         property.setUsername(argsObj.user);
         property.setPassword(argsObj.password);
         switch (argsObj.type) {
             case 1:
                 property.setDriver(DBDriver.MYSQL8.driverClass);
+                property.setJdbcUrl(String.format(DBDriver.MYSQL5.jdbcUrl, argsObj.host, argsObj.port, argsObj.db));
                 break;
             case 2:
                 property.setDriver(DBDriver.ORACLE.driverClass);
+                property.setJdbcUrl(String.format(DBDriver.ORACLE.jdbcUrl, argsObj.host, argsObj.port, argsObj.db));
                 break;
             case 3:
                 property.setDriver(DBDriver.SQLSERVER.driverClass);
+                property.setJdbcUrl(String.format(DBDriver.SQLSERVER.jdbcUrl, argsObj.host, argsObj.port, argsObj.db));
                 break;
             default:
                 property.setDriver(DBDriver.PSQL.driverClass);
-                break;
+                property.setJdbcUrl(String.format(DBDriver.PSQL.jdbcUrl, argsObj.host, argsObj.port, argsObj.db));
         }
-        DBSize dbSize = dbMonitor.queryDBSize(property);
+        DBSize dbSize = monitor.queryDBSize(property);
         List<TableSize> detailSize = dbSize.getDetailSize();
         log.info("查询该数据库共有{}张表", detailSize.size());
         for (int i = 0; i < detailSize.size(); i++) {
             TableSize tableSize = detailSize.get(i);
             Database.Table table = new Database.Table();
             table.setTableName(tableSize.getTableName());
-            TableStructure tableStructure = tbMonitor.queryTableStructure(property, tableSize.getTableName());
+            TableStructure tableStructure = monitor.queryTableStructure(property, tableSize.getTableName());
             List<ColumnInfo> columnInfos = tableStructure.getColumnInfos();
             if (columnInfos != null && columnInfos.size() != 0) {
                 for (ColumnInfo columnInfo : columnInfos) {
